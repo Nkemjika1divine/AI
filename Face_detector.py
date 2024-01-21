e253dfdfe532267301ed4a1ccf9ab4326e9289c1
Nkemjika1divine <cndivine@gmail.com>
2024-01-21 12:19:02 +0100

    Printing the coordinates of the face

--- Face_detector.py
+++ Face_detector.py
@@ -10,12 +10,13 @@ image = cv2.imread("CE.jpg")
 # change it to grayscale using cvtColor
 gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-
-
+# This ensures that the coordinates of the faces in the images are recognized and stored no matter the size
+face_cordinates = frontface.detectMultiScale(gray_image)
+print(face_cordinates)
 
 
 # to show the picture
-cv2.imshow("This is our Photo", gray_image)
+# cv2.imshow("This is our Photo", gray_image)
 
 # this is used to make the program wait for execution. it keeps the image open for viewing
 cv2.waitKey()
