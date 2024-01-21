937fcc6cc8dc634547b31691dff6788cf70c935f
Nkemjika1divine <cndivine@gmail.com>
2024-01-21 02:28:48 -0800

    AI Learning

--- Face_detector.py
+++ Face_detector.py
@@ -2,15 +2,16 @@
 import cv2
 
 # open the haarcascade_frontalface_default.xml file
-with cv2.CascadeClassifier("frontalface_default.xml") as frontface:
-    # import the image usin cv2.imread
-    image = cv2.imread("CE.jpg")
+frontface = cv2.CascadeClassifier("frontalface_default.xml")
 
-    # to show the picture
-    cv2.imshow("This is our Photo", image)
+# import the image usin cv2.imread
+image = cv2.imread("CE.jpg")
 
-    # this is used to make the program wait for execution
-    cv2.waitkey()
+# to show the picture
+cv2.imshow("This is our Photo", image)
+
+# this is used to make the program wait for execution
+cv2.waitkey()
 
 
 
