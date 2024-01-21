7e684644e559169d15b9a44b74393dcc4a63292b
Nkemjika1divine <cndivine@gmail.com>
2024-01-21 12:02:41 +0100

    Grayscale effect

--- Face_detector.py
+++ Face_detector.py
@@ -7,10 +7,17 @@ frontface = cv2.CascadeClassifier("frontalface_default.xml")
 # import the image usin cv2.imread
 image = cv2.imread("CE.jpg")
 
-# to show the picture
-cv2.imshow("This is our Photo", image)
+# change it to grayscale using cvtColor
+gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-# this is used to make the program wait for execution
+
+
+
+
+# to show the picture
+cv2.imshow("This is our Photo", gray_image)
+
+# this is used to make the program wait for execution. it keeps the image open for viewing
 cv2.waitKey()
 
 
