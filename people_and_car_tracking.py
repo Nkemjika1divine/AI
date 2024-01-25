9badecb6b59c169142882c095fca8978f0eb08b4
Nkemjika1divine <cndivine@gmail.com>
2024-01-25 04:42:27 +0100

    set it up to show images in grayscale

--- people_and_car_tracking.py
+++ people_and_car_tracking.py
@@ -1,2 +1,10 @@
 import cv2
 
+human_body = cv2.CascadeClassifier("fullbody.xml")
+
+image = cv2.imread("avengers.jpg")
+
+gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+cv2.imshow("The Image", gray_image)
+cv2.waitKey()
